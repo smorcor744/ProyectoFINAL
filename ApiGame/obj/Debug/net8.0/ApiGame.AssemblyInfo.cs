3522c55ad716e3c86e1a2c74160cb856ac8ffa1e
smorcor744 <mcsergio@iesrafaelalberti.es>
2025-03-11 11:38:28 +0100

    Api terminada score = Tiempo en derrotar al rino

--- ApiGame/obj/Debug/net8.0/ApiGame.AssemblyInfo.cs
+++ ApiGame/obj/Debug/net8.0/ApiGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7667b68beabb6476f9bf8f7dd558354b5d910cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
